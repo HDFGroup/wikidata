4d3231dda70cdb5fd06698d14b543324e45a3d3a
Gerd Heber <gheber@hdfgroup.org>
2025-02-18 20:16:16 +0000

    Add a few properties.

--- examples/ontology.py
+++ examples/ontology.py
@@ -16,6 +16,15 @@ g.add((H5.Datatype, RDF.type, RDFS.Class))
 g.add((H5.Dataspace, RDF.type, RDFS.Class))
 g.add((H5.Link, RDF.type, RDFS.Class))
 
+g.add((H5.Object, RDF.type, RDFS.Class))
+g.add((H5.Group, RDFS.subClassOf, H5.Object))
+g.add((H5.Dataset, RDFS.subClassOf, H5.Object))
+g.add((H5.DatatypeObject, RDFS.subClassOf, H5.Object))
+
+g.add((H5.ArrayVariable, RDF.type, RDFS.Class))
+g.add((H5.Attribute, RDFS.subClassOf, H5.ArrayVariable))
+g.add((H5.Dataset, RDFS.subClassOf, H5.ArrayVariable))
+
 #===============================================================================
 # Labels
 #===============================================================================
@@ -29,6 +38,9 @@ g.add((H5.Datatype, RDFS.label, Literal("HDF5 Datatype")))
 g.add((H5.Dataspace, RDFS.label, Literal("HDF5 Dataspace")))
 g.add((H5.Link, RDFS.label, Literal("HDF5 Link")))
 
+g.add((H5.Object, RDFS.label, Literal("HDF5 Object")))
+g.add((H5.ArrayVariable, RDFS.label, Literal("HDF5 ArrayVariable")))
+
 #===============================================================================
 # Properties
 #===============================================================================
@@ -40,6 +52,23 @@ g.add((H5.hasRoot, RDFS.range, H5.Group))
 g.add((H5.hasLink, RDF.type, RDF.Property))
 g.add((H5.hasLink, RDFS.domain, H5.Group))
 g.add((H5.hasLink, RDFS.range, H5.Link))
+
+g.add((H5.hasLink, RDF.type, RDF.Property))
+g.add((H5.hasLink, RDFS.domain, H5.Group))
+g.add((H5.hasLink, RDFS.range, H5.Link))
+
+g.add((H5.destination, RDF.type, RDF.Property))
+g.add((H5.destination, RDFS.domain, H5.Link))
+
+g.add((H5.hasAttribute, RDF.type, RDF.Property))
+g.add((H5.hasAttribute, RDFS.domain, H5.Object))
+g.add((H5.hasAttribute, RDFS.range, H5.Attribute))
+
+g.add((H5.hasDataspace, RDF.type, RDF.Property))
+g.add((H5.hasDataspace, RDFS.domain, H5.ArrayVariable))
+
+g.add((H5.hasDatatype, RDF.type, RDF.Property))
+g.add((H5.hasDatatype, RDFS.domain, H5.ArrayVariable))
 
 #===============================================================================
 
